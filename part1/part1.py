ec4edc2dd6e8aee833ea19b4a3a4fd0c85a60503
AdrianCCo <adrianc.co@outlook.com>
2024-01-30 15:23:34 -0700

    Did some of part 1

--- part1/part1.py
+++ part1/part1.py
@@ -1,5 +1,12 @@
-def hello_world(name):
-    
-    print("Hello, world! My name is", name)
+#1. Checks if the if input is 1 or 0, if so then it will return the value of n
+#   If the value is neither of the above, it will call the function twice recursively where the input is n - 1 
+#   and n - 2 where they are added together and returned. Once n reaches 1, the function will return a value until
+#   it reaches the initial caller of the function.
 
-hello_world("Adrian Co")
+#2. No, this is not a divide and conquer algorithm
+
+
+#3. The big O notation for the recursive fibonacci function is O(2n). This is because for each input of n, the 
+#   function will call the function recursively twice more until the input reaches 0 or 1
+
+
