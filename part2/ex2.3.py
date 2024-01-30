a9a24e1ceb8bfbfd9c8c916a614867d511651bc9
AdrianCCo <adrianc.co@outlook.com>
2024-01-29 17:51:49 -0700

    changed ex2.3

--- part2/ex2.3.py
+++ part2/ex2.3.py
@@ -1,7 +1,7 @@
 import json
 
 #read file
-f = open('large-file.json', 'r', encoding = "utf8")
+f = open('part2\large-file.json', 'r', encoding = "utf8")
 data = json.load(f)
 f.close
 
@@ -12,6 +12,6 @@ for i in range(len(data)):
 
 data.reverse()
 
-f = open('output.2.3.json', 'w', encoding = "utf8")
+f = open('part2\output.2.3.json', 'w', encoding = "utf8")
 f.write(json.dumps(data))
 f.close()
