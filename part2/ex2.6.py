ae6a720af77a3aaaf9c300ca273e997e292b18d3
AdrianCCo <adrianc.co@outlook.com>
2024-01-29 18:03:33 -0700

    changed ex2.6

--- part2/ex2.6.py
+++ part2/ex2.6.py
@@ -1,4 +1,7 @@
 import timeit
+import sys
+
+sys.setrecursionlimit(10500)
 
 def for_pow2(n):
     num = 1
@@ -11,9 +14,17 @@ def listComp_pow2(n):
     result = [2**num for num in range(n + 1)]
     return result[n]
 
-print("For Loop Time Taken:", timeit.timeit(stmt = "for_pow2(1000)", setup = "from __main__ import for_pow2"), "seconds")
+def pow2(n):
+    if(n == 0):
+        return 1
+    return 2 * pow2(n - 1)
 
-print("List Comprehension Time Taken:", timeit.timeit(stmt = "listComp_pow2(1000)", setup = "from __main__ import listComp_pow2"), "seconds")
+
+print("pow2(10000) Function Time Taken to execute 10000 times:", timeit.timeit(stmt = "for_pow2(10000)", setup = "from __main__ import for_pow2", number = 10000), "seconds")
+
+print("for_pow2(1000) Function Time Taken to execute 1000 times:", timeit.timeit(stmt = "for_pow2(1000)", setup = "from __main__ import for_pow2", number = 1000), "seconds")
+
+print("listComp_pow2(1000) Function Time Taken to execute 1000 times:", timeit.timeit(stmt = "listComp_pow2(1000)", setup = "from __main__ import listComp_pow2", number = 1000), "seconds")
 
 
 
